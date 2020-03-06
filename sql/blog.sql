d509866ed3b35e535b19a9fc9af6ba4d959c9423
何予風語 <ipprologue@gmail.com>
2020-03-06 16:13:10 +0800

    feat: 创建tag表

---
+++ sql/blog.sql
@@ -0,0 +1,13 @@
+
+
+CREATE TABLE `blog_tag` (
+    `id` int(10) unsigned NOT NULL  AUTO_INCREMENT,
+    `name` varchar(100) DEFAULT '' COMMENT '标签名称',
+    `created_on` int(10) unsigned DEFAULT '0' COMMENT '创建时间',
+    `created_by` varchar(100) default '' COMMENT '创建人',
+    `modified_on` int(10) unsigned DEFAULT '0' COMMENT '修改时间',
+    `modified_by` varchar(100) default '' COMMENT '修改人',
+    `deleted_on` int(10) unsigned DEFAULT '0',
+    `state` tinyint(3) unsigned DEFAULT '1'  COMMENT '状态 0禁用、 1启用',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT  CHARSET = utf8 COMMENT ='文章标签页'
