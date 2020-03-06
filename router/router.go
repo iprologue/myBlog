ff9d1cac8e062ce6773acd9be1790eafd1a5b94a
何予風語 <ipprologue@gmail.com>
2020-03-06 21:47:08 +0800

    feat: 实现文章接口

--- router/router.go
+++ router/router.go
@@ -13,5 +13,11 @@ func InitRouter(r *gin.Engine)  {
 		GroupV1.POST("/tags", v1.AddTag)
 		GroupV1.PUT("/tags/:id", v1.EditTag)
 		GroupV1.DELETE("/tags/:id", v1.DeletedTag)
+
+		GroupV1.GET("/articles", v1.GetArticles)
+		GroupV1.GET("/articles/:id", v1.GetArticle)
+		GroupV1.POST("/articles", v1.AddArticle)
+		GroupV1.PUT("/articles/:id", v1.EditArticle)
+		GroupV1.DELETE("/articles/:id", v1.DeleteArticle)
 	}
 }
