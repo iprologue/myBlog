be4537690f869b8126c45d4d000a991505a8deee
何予風語 <ipprologue@gmail.com>
2020-03-08 22:05:18 +0800

    refactor: 优化auth接口

--- models/auth.go
+++ models/auth.go
@@ -1,12 +1,12 @@
 package models
 
 type Auth struct {
-	ID int `gorm:"primary_key" json:"id"`
+	ID       int    `gorm:"primary_key" json:"id"`
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
 
-func CheckAuth(username, password string) (bool, error)  {
+func CheckAuth(username, password string) (bool, error) {
 	var auth Auth
 	err := db.Select("id").Where(Auth{Username: username, Password: password}).First(&auth).Error
 	if err != nil {
--- router/api/auth.go
+++ router/api/auth.go
@@ -5,8 +5,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/iprologue/myBlog/common/errcode"
 	"github.com/iprologue/myBlog/common/util"
-	"github.com/iprologue/myBlog/models"
-	"log"
+	"github.com/iprologue/myBlog/pkg/app"
+	"github.com/iprologue/myBlog/service"
 	"net/http"
 )
 
@@ -16,38 +16,39 @@ type auth struct {
 }
 
 func GetAuth(c *gin.Context)  {
+	appG := app.Gin{c}
+	valid := validation.Validation{}
+
 	username := c.Query("username")
 	password := c.Query("password")
 
-	valid := validation.Validation{}
 	a := auth{Username: username, Password: password}
 	ok, _ := valid.Valid(&a)
 
-	data := make(map[string]interface{})
-	code := errcode.INVALID_PARAMS
-	if ok {
-		if checkAuth, _ := models.CheckAuth(username, password); checkAuth {
-			token, err := util.CreateToken(username, password)
-			if err != nil {
-				log.Println(err.Error())
-				code = errcode.ERROR_AUTH_TOKEN
-			} else {
-				data["token"] = token
-
-				code = errcode.SUCCESS
-			}
-		} else {
-			code = errcode.ERROR_AUTH
-		}
-	} else  {
-		for _, err := range valid.Errors {
-			log.Println(err.Key, err.Message)
-		}
+	if !ok {
+		app.MarkErrors(valid.Errors)
+		appG.Response(http.StatusBadRequest, errcode.INVALID_PARAMS, nil)
+		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{
-		"code": code,
-		"msg": errcode.GetMsg(code),
-		"data": data,
+	authService := service.Auth{Username: username, Password: password}
+	exist, err := authService.Check()
+	if err != nil {
+		appG.Response(http.StatusInternalServerError, errcode.ERROR_AUTH_CHECK_TOKEN_FAIL, nil)
+		return
+	}
+	if !exist {
+		appG.Response(http.StatusUnauthorized, errcode.ERROR_AUTH, nil)
+		return
+	}
+
+	token, err := util.CreateToken(username, password)
+	if err != nil {
+		appG.Response(http.StatusInternalServerError, errcode.ERROR_AUTH_TOKEN, nil)
+		return
+	}
+
+	appG.Response(http.StatusOK, errcode.SUCCESS, map[string]string{
+		"token":token,
 	})
 }
